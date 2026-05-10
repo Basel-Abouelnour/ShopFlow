5b6b876dcc188558d10c4e89d020f6a522331e6a
Basel Abouelnour <basekabouelnour@gmail.com>
2026-05-10 10:26:42 +0000

    added output

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,8 +1,12 @@
-output "website_url" {
+output "alb_dns" {
   value       = "http://${module.compute.alb_dns_name}"
   description = "Copy and paste this into your browser"
 }
 output "asg_name" {
   description = "Auto Scaling Group name for instance refresh on deploy"
   value       = module.compute.asg_name
+}
+output "ecr_url" {
+  description = "URL for the created ecr repo"
+  value       = module.ecr.repository_url
 }
